8d789208ebf760942ea8ee9261d16c7547396e7e
Dima163163 <khlopovdima@gmail.com>
2023-09-26 14:11:31 +0400

    const and class modal

---
+++ js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const title = document.querySelector('.modal__title');
+const close = document.querySelector('.modal-close');
+const id = document.querySelector('.modal__id');
+const idNumber = document.querySelector('.modal__id-number');
+const form = document.querySelector('.form');
+const checkbox = document.querySelector('.crm__checkbox');
+const checkboxInput = document.querySelector('.crm__discont-input');
+const totalTitle = document.querySelector('.total-cost');
+const totalPrice = document.querySelector('.total-cost__span');
+
+modal.classList.add('modal-open');
+
+console.log(modal);
+console.log(title);
+console.log(close);
+console.log(id);
+console.log(idNumber);
+console.log(form);
+console.log(checkbox);
+console.log(checkboxInput);
+console.log(totalTitle);
+console.log(totalPrice);
