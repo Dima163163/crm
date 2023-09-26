11ed8a205df958334f9c85fa83cff3a7a3b9369a
Dima163163 <khlopovdima@gmail.com>
2023-09-26 13:41:45 +0400

    init dev

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const nameProduct = 'JBL колонка';
+const amountProduct = 4;
+const categoryProduct = 'аудио техника';
+const priceProduct = 12000;
+
+console.log('Наименование товара: ' + nameProduct);
+console.log('Общая сумма товара: ' + (amountProduct * priceProduct))
