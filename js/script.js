3a101adb6865effb294fd1a0cf88be352834d63f
Dima163163 <khlopovdima@gmail.com>
2023-09-27 12:22:39 +0400

    merge dom2

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const title = document.querySelector('.modal__title');
+const close = document.querySelector('.modal-close');
+const id = document.querySelector('.modal__id');
+const idNumber = document.querySelector('.modal__id-number');
+const form = document.querySelector('.form');
+const checkbox = document.querySelector('.crm__checkbox');
+const checkboxInput = document.querySelector('.crm__discont-input');
+const totalTitle = document.querySelector('.total-cost');
+const totalPrice = document.querySelector('.total-cost__span');
+
+modal.classList.add('modal-open');
+const modalOpen = document.querySelector('.modal-open');
+
+modalOpen.style = 'display: flex;'
+
+console.log(modal);
+console.log(title);
+console.log(close);
+console.log(id);
+console.log(idNumber);
+console.log(form);
+console.log(checkbox);
+console.log(checkboxInput);
+console.log(totalTitle);
+console.log(totalPrice);
