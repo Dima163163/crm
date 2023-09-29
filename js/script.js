c446f68efb0de6da1244b83fa751d9360f0bcf8c
Dima163163 <khlopovdima@gmail.com>
2023-09-29 14:55:46 +0400

    DOM5 event

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const modal = document.querySelector('.modal');
 const title = document.querySelector('.modal__title');
-const close = document.querySelector('.modal-close');
+const closeBtn = document.querySelector('.modal-close');
 const id = document.querySelector('.modal__id');
 const idNumber = document.querySelector('.modal__id-number');
 const form = document.querySelector('.form');
@@ -10,21 +10,27 @@ const checkbox = document.querySelector('.crm__checkbox');
 const checkboxInput = document.querySelector('.crm__discont-input');
 const totalTitle = document.querySelector('.total-cost');
 const totalPrice = document.querySelector('.total-cost__span');
+const addProduct = document.querySelector('.cms-btn');
+const overlayModal = document.querySelector('.overlay');
 
+// Функция открытия модального окна
+const openModal = () => {
+  modal.classList.add('modal-open');
+  const modalOpen = document.querySelector('.modal-open');
 
-modal.classList.add('modal-open');
-const modalOpen = document.querySelector('.modal-open');
+  overlayModal.style = 'opacity: 1; visibility: visible; display: flex;';
+  modalOpen.style = 'display: flex;'
+};
 
-modalOpen.style = 'display: flex;'
+// Функция закрытия модального окна
+const closeModal = () => {
+  overlayModal.style = 'opacity: 0; visibility: hidden; display: none;';
+  modal.classList.remove('modal-open');
+}
 
-
-console.log(modal);
-console.log(title);
-console.log(close);
-console.log(id);
-console.log(idNumber);
-console.log(form);
-console.log(checkbox);
-console.log(checkboxInput);
-console.log(totalTitle);
-console.log(totalPrice);
+modal.addEventListener('click', event => {
+  event.stopImmediatePropagation();
+})
+addProduct.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+overlayModal.addEventListener('click', closeModal);
