a1e6e7c1d99eb4a486a4f782a17bf1e491948632
Dima163163 <khlopovdima@gmail.com>
2023-11-06 17:22:52 +0400

    fix id add goods pages fix console

--- js/modules/editGood.js
+++ js/modules/editGood.js
@@ -4,7 +4,6 @@ const editGood = (tableList, fetchRequest, renderGoods,
   tableList.addEventListener('click', async (e) => {
     const target = e.target;
     if (target.classList.contains('td-button-edit')) {
-      console.log('target.dataset.id: ', target.dataset.id);
       await fetchRequest(`/api/goods/${target.dataset.id}`, {
         callback: (err, data) => {
           showModal(err, data);
